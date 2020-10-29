7b7da3b4bd5f1983c9964bfab678aef5a30d3e96
ssv-170379 <ssv_170379@ukr.net>
2020-10-29 10:27:10 +0200

    4.1.7 - my very first Page object function

--- test_main_page.py
+++ test_main_page.py
@@ -1,10 +1,14 @@
 from os import system
 
-def test_guest_can_go_to_login_page(browser):
-	link = "http://selenium1py.pythonanywhere.com/"
-	browser.get(link)
+link = "http://selenium1py.pythonanywhere.com/"
+
+def go_to_login_page(browser):
 	login_link = browser.find_element_by_css_selector("#login_link")
 	login_link.click()
+
+def test_guest_can_go_to_login_page(browser): 
+	browser.get(link)
+	go_to_login_page(browser)
 
 def main():
 	system(f'pytest -v --tb=line --language=en "{__file__}"')
