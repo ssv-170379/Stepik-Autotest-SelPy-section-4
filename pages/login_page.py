71da946972f10a6c0588b332ffced939eb04960e
ssv-170379 <ssv_170379@ukr.net>
2020-10-31 13:50:07 +0200

    4.2.9 - fixed mistakes in login page tests, added code to go to another page in current tab

--- pages/login_page.py
+++ pages/login_page.py
@@ -11,11 +11,11 @@ class LoginPage(BasePage):
 
 	def should_be_login_url(self):
 		# реализуйте проверку на корректный url адрес
-		assert 'login' in browser.current_url, "the Login page address is not found on the page"
+		assert 'login' in self.browser.current_url, "the 'Login page' URL does not include the 'login' substring"
 
 	def should_be_login_form(self):
-		assert self.is_element_present(*MainPageLocators.LOGIN_FORM), "the Login form is not found on the page"
+		assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "the 'Login form' is not found on the 'Login page'"
 
 	def should_be_register_form(self):
-		assert self.is_element_present(*MainPageLocators.REGISTRSTION_FORM), "the Registration form is not found on the page"
+		assert self.is_element_present(*LoginPageLocators.REGISTRSTION_FORM), "the 'Registration form' is not found on the 'Login page'"
 
